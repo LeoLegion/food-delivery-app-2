5e287c63a8d603a2ac1063ccebb4aa51dcb445d0
LeoLegion <monkeydluffyonepiece114@gmail.com>
2026-04-19 19:38:53 +0530

    Add TCP client to communicate with server

--- client/client.c
+++ client/client.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include "client.h"
-#include "../common/protocol.h"
+// #include "client.h"
+// #include "../common/protocol.h"
+
+#define PORT 8080
 
 int main() {
     int sock;
     struct sockaddr_in server;
-    Request req;
-    Response res;
+    // Request req;
+    // Response res;
+    char buffer[256];
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -18,14 +23,29 @@ int main() {
 
     connect(sock, (struct sockaddr*)&server, sizeof(server));
 
-    while (1) {
-        printf("\n1.Register 2.Login 3.Exit\n");
-        scanf("%d", &req.command);
-        send(sock, &req, sizeof(req), 0);
-        recv(sock, &res, sizeof(res), 0);
+    printf("Connected to server. Type messages (type 'exit' to quit)\n");
 
-        printf("Server: %s\n", res.message);
+    while (1) {
+        // printf("\n1.Register 2.Login 3.Exit\n");
+        // scanf("%d", &req.command);
+        // send(sock, &req, sizeof(req), 0);
+        // recv(sock, &res, sizeof(res), 0);
+
+        // printf("Server: %s\n", res.message);
+        printf("> ");
+        fgets(buffer, sizeof(buffer), stdin);
+
+        if (strncmp(buffer, "exit", 4) == 0)
+            break;
+
+        send(sock, buffer, strlen(buffer), 0);
+
+        int n = recv(sock, buffer, sizeof(buffer) - 1, 0);
+        buffer[n] = '\0';
+
+        printf("Server replied: %s", buffer);
     }
 
     close(sock);
+    return 0;
 }
